c993b66bd4d4263c51dd46177dd378df1b005be8
harshabandaru <bandarusriharsha@gmail.com>
2014-02-11 15:36:21 +0530

    Done and Submission Completed

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lex.h"
+#include "symbol.h"
+
+
+extern yyleng;
+extern char * yytext;
+
+void main(){
+
+st sym_tab;
+sym_tab.cur_size=0;
+int tok;
+
+while( (tok=lex()) != EOI){
+	if(tok == ERR)	{
+		//printf("Invalid Lexeme %0.*s at line: %d\n",yyleng, yytext, yylineno);
+	}
+}
+
+printf("Symbol Table\n\n");
+pprint();
+
+}
